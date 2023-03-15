8edc3d000c9a0bd6b51d64de455eaf1ed1eeb831
Max Lobur <max_lobur@outlook.com>
2023-03-15 11:49:36 +0200

    Split diffs

--- script.sh
+++ script.sh
@@ -7,11 +7,14 @@ fi
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-TMPFILE=$(mktemp)
-git diff >"${TMPFILE}"
+DEL=$(mktemp)
+ADD=$(mktemp)
+git diff | grep -v "^+[^+]" >"${DEL}"
+git diff | grep -v "^-[^-]" >"${ADD}"
 
 git stash -u
 
+# report deletions
 reviewdog \
   -name="${INPUT_TOOL_NAME:-reviewdog-suggester}" \
   -f=diff \
@@ -20,7 +23,18 @@ reviewdog \
   -filter-mode="${INPUT_FILTER_MODE}" \
   -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
   -level="${INPUT_LEVEL}" \
-  ${INPUT_REVIEWDOG_FLAGS} <"${TMPFILE}" || EXIT_CODE=$?
+  ${INPUT_REVIEWDOG_FLAGS} <"${DEL}" || EXIT_CODE=$?
+
+# report additions
+reviewdog \
+  -name="${INPUT_TOOL_NAME:-reviewdog-suggester}" \
+  -f=diff \
+  -f.diff.strip=1 \
+  -reporter="github-pr-review" \
+  -filter-mode="${INPUT_FILTER_MODE}" \
+  -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
+  -level="${INPUT_LEVEL}" \
+  ${INPUT_REVIEWDOG_FLAGS} <"${ADD}" || EXIT_CODE=$?
 
 if [ "${INPUT_CLEANUP}" = "true" ]; then
   git stash drop || true
